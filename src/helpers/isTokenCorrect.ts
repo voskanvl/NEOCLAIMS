2972a624ed1fe7101c60778b7580ffca674699c9
voskanvl <voskanvl@gmail.com>
2022-05-30 22:30:11 +0300

    fix isTokenCorrect

--- src/helpers/isTokenCorrect.ts
+++ src/helpers/isTokenCorrect.ts
@@ -1,9 +1,9 @@
 export function isTokenCorrect(): boolean {
     const token: string | null = localStorage.getItem("token");
     const created = localStorage.getItem("created");
-    if (!localStorage || !created) return false;
+    if (!token || !created) return false;
     const middle = token!.split(".")[1];
     const toketJson = JSON.parse(atob(middle));
     const expired = +created + toketJson.exp - toketJson.iat;
-    return Date.now() < expired;
+    return Date.now() / 1000 < expired;
 }
