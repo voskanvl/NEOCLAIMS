539c617a1bdc94115cd9dea8690548ad2d9b90d8
voskanvl <voskanvl@gmail.com>
2022-05-30 22:31:09 +0300

    add type claim in claim page

--- src/pages/Claim.tsx
+++ src/pages/Claim.tsx
@@ -1,22 +1,31 @@
-import { FC, useEffect } from "react"
+import { FC, useEffect, useState } from "react"
 import { useDispatch } from "react-redux"
 import { useParams } from "react-router-dom"
 import { currentClaimFetch } from "../app/claim"
 import { useAppDispatch, useAppSelector } from "../app/hooks"
+import { typeFetch } from "../app/type"
 import { Input } from "../features/input/Input"
 import { isTokenCorrect } from "../helpers/isTokenCorrect"
 
 export const Claim: FC = (props) => {
+    const [title, setTitle] = useState('')
+    const [typeVal, setTypeVal] = useState('')
+    const [description, setDescription] = useState('')
     const { claimId } = useParams()
     const { currentClaim } = useAppSelector(state => state.currentClaim)
+    const { type } = useAppSelector(state => state.type)
     const dispatch = useAppDispatch()
     useEffect(() => {
         if (isTokenCorrect() && claimId) dispatch(currentClaimFetch(claimId))
+        if (isTokenCorrect() && claimId) dispatch(typeFetch())
     }, [dispatch])
 
     return <>
-        <Input label={"title"} value={currentClaim.title} />
-        <Input label={"type"} value={currentClaim.type.name} />
-        <Input label={"description"} value={currentClaim.description} />
+        <Input label={"title"} value={currentClaim.title} onChange={ev => setTitle(ev.currentTarget.value)} />
+        <Input label={"type"} value={currentClaim.type.name} onChange={ev => setTypeVal(ev.currentTarget.value)} />
+        <select>
+            {type.map(el => <option key={el.slug} value={el.name} defaultChecked={el.name === currentClaim.type.name}>{el.name}</option>)}
+        </select>
+        <Input label={"description"} value={currentClaim.description} onChange={ev => setDescription(ev.currentTarget.value)} />
     </>
 }
