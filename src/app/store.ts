cd983151445eb09ba61e004b9ee905ded0ae7046
voskanvl <voskanvl@gmail.com>
2022-05-30 22:30:40 +0300

    type Slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,12 +3,14 @@ import counterReducer from "../features/counter/counterSlice";
 import { loginSlice } from "./login";
 import { claimsSlice } from "./claims";
 import { currentClaimSlice } from "./claim";
+import { typeSlice } from "./type";
 
 export const store = configureStore({
     reducer: {
         login: loginSlice.reducer,
         claims: claimsSlice.reducer,
         currentClaim: currentClaimSlice.reducer,
+        type: typeSlice.reducer,
         counter: counterReducer,
     },
 });
---
+++ src/app/type.ts
@@ -0,0 +1,37 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const typeFetch = createAsyncThunk("type/fetch", async () => {
+    try {
+        const token = localStorage.getItem("token");
+        const response = await fetch(`http://macserver.local:3001/types`, {
+            headers: {
+                "Authorization": "Bearer " + token,
+                "Content-Type": "application/json",
+            },
+            mode: "cors",
+        });
+        const result = await response.json();
+        console.log("🚀 ~ result typeFetch", result);
+        return result;
+    } catch (error) {
+        return error;
+    }
+});
+export const typeSlice = createSlice({
+    name: "type",
+    initialState: {
+        type: [] as { name: string; slug: string }[],
+        error: "",
+    },
+    reducers: {},
+    extraReducers: builder => {
+        builder.addCase(typeFetch.fulfilled, (state, action) => {
+            console.log("🚀 ~ action typeFetch", action);
+            if ("message" in action.payload && "code" in action.payload) {
+                state.error = action.payload.error;
+            } else {
+                state.type = action.payload;
+            }
+        });
+    },
+});
