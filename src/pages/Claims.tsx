285cabf9f4fb54caf977c52faa49fcd9b3f5b654
voskanvl <voskanvl@gmail.com>
2022-06-01 08:22:57 +0300

    continue sort, switch method sorting

--- src/pages/Claims.tsx
+++ src/pages/Claims.tsx
@@ -8,6 +8,7 @@ import style from "./claims.module.sass"
 
 export const Claims: FC = (props) => {
     const [claims, setClaims] = useState<TClaim[]>()
+    const [didSort, setDidSort] = useState<{ attribute: keyof TClaim, method: 'asc' | 'desc' }>()
     const claimsFromServer = useAppSelector(state => state.claims.claims)
     const dispatch = useAppDispatch()
     const navigate = useNavigate()
@@ -22,13 +23,32 @@ export const Claims: FC = (props) => {
         setClaims(claimsFromServer)
     }, [claimsFromServer])
     const sort = (field: keyof TClaim): void => {
-        setClaims(
-            [...claimsFromServer].sort((a, b) => {
-                if (a[field] > b[field]) { return 1 }
-                if (a[field] < b[field]) { return -1 }
-                return 0
-            })
-        )
+        const asc = (field: keyof TClaim) => [...claimsFromServer].sort((a, b) => {
+            if (a[field] > b[field]) { return 1 }
+            if (a[field] < b[field]) { return -1 }
+            return 0
+        })
+        const desc = (field: keyof TClaim) => [...claimsFromServer].sort((a, b) => {
+            if (a[field] < b[field]) { return 1 }
+            if (a[field] > b[field]) { return -1 }
+            return 0
+        })
+        if (didSort?.attribute === field && didSort?.method === 'desc') {
+            setClaims(asc(field))
+            setDidSort({ attribute: field, method: 'asc' })
+            return
+        }
+        if (didSort?.attribute === field && didSort?.method === 'asc') {
+            setClaims(desc(field))
+            setDidSort({ attribute: field, method: 'desc' })
+            return
+        }
+        if (didSort?.attribute !== field) {
+            setClaims(asc(field))
+            setDidSort({ attribute: field, method: 'asc' })
+            return
+        }
+
     }
     return <>
         <div className={style.table}>
@@ -37,7 +57,7 @@ export const Claims: FC = (props) => {
                 <button className={`${style.table__button} ${style.table__button_createAt}`} onClick={() => sort('createdAt')}>Created</button>
                 <button className={`${style.table__button} ${style.table__button_type}`} onClick={() => sort('type')}>Type</button>
                 <button className={`${style.table__button} ${style.table__button_status}`} onClick={() => sort('status')}>Status</button>
-                <button className={`${style.table__button} ${style.table__button_actioins}`} >Action</button>
+                <button className={`${style.table__button} ${style.table__button_actions}`} >Action</button>
             </div>
             {(claims && claims.length) && claims.map((el: TClaim) => <div key={el._id} className={style.row}>
                 <div>{el.title}</div>
