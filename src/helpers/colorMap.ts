4210bc27f8c9d67a1a6abeb823fd517657659215
voskanvl <voskanvl@gmail.com>
2022-06-03 19:49:04 +0300

    colorMap

---
+++ src/helpers/colorMap.ts
@@ -0,0 +1,8 @@
+const colorMap: { type: string; slug: string; color: string }[] = [
+    { type: "Hardware", slug: "hard", color: "#7DB59A" },
+    { type: "Software", slug: "soft", color: "#FF7675" },
+    { type: "Software", slug: "soft", color: "#FF7675" },
+    { type: "Networking", slug: "net", color: "#FDCB6E" },
+    { type: "Troubleshooting", slug: "troublesh", color: "#6C5CE7" },
+];
+export default colorMap;
