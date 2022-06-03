bfcb0ace215b085bdeefc7f73e24639138057d1c
voskanvl <voskanvl@gmail.com>
2022-06-03 19:49:36 +0300

    fix shallowFlat

--- src/helpers/shallowFlat.ts
+++ src/helpers/shallowFlat.ts
@@ -1,6 +1,6 @@
 export function shallowFlat(x: object, name: string) {
     return Object.entries(x).reduce((acc, [key, value]) => {
-        if (typeof value === "object") value = value[name];
+        if (value && typeof value === "object") value = value[name];
         return { ...acc, [key]: value };
     }, {});
 }
