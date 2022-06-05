03be028251e2cae7713ff9a12ccaf2beea601fed
voskanvl <voskanvl@gmail.com>
2022-06-05 06:33:14 +0300

    logout

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,4 +1,5 @@
 import { FC, memo, ReactNode, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 import { useAppDispatch, useAppSelector } from "../../app/hooks"
 import { currentUser } from "../../app/login"
 import { show } from "../../app/showAside"
@@ -8,6 +9,7 @@ import style from "./header.module.sass"
 const Header: FC<{ children: ReactNode }> = ({ children }) => {
     let userName = useAppSelector(state => state.login.user.fullName)
     const dispatch = useAppDispatch()
+    const navigate = useNavigate()
     useEffect(() => {
         const token = localStorage.getItem('token')
         if (!userName) {
@@ -17,13 +19,18 @@ const Header: FC<{ children: ReactNode }> = ({ children }) => {
         }
     }, [userName])
 
+    const logout = () => {
+        localStorage.clear()
+        navigate('/')
+    }
+
     return <header className={style.header}>
         <button className={style.burger} onClick={() => dispatch(show())}>{svg.burger}</button>
         <div className={style.children}>{children}</div>
         <div className={style.bell}>{svg.bell}</div>
         <div className={style.face}></div>
         <div className={style.name}>{userName}</div>
-        <div className={style.logout}>{svg.logout}</div>
+        <button className={style.logout} onClick={logout}>{svg.logout}</button>
     </header>
 }
 
