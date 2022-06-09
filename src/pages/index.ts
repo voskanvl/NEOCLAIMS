bef5ccee5d8c741652d0b3c60daf6d3477bf3705
voskanvl <voskanvl@gmail.com>
2022-06-09 19:32:55 +0300

    rmove Login & Reg

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,8 +1,6 @@
-import { Reg } from "./Reg";
 import { Claim } from "./Claim";
 import { Claims } from "./Claims";
-import { Login } from "./Login";
 import { LoginReg } from "./LoginReg";
 import { Create } from "./Create";
 
-export default { Reg, Claim, Claims, Create, Login, LoginReg };
+export default { Claim, Claims, Create, LoginReg };
