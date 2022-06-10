3d9976a89ef8fb90f691f01ce344ee413a661871
voskanvl <voskanvl@gmail.com>
2022-06-10 21:50:07 +0300

    fix dtyle in error500

--- src/features/Error/Error500.tsx
+++ src/features/Error/Error500.tsx
@@ -1,7 +1,7 @@
 import { memo } from "react"
 import style from "./error500.module.sass"
-export const Error500 = memo(() => <div className={style.container}>
-    <h1>500</h1>
-    <p>Server is not available</p>
-    <p>try reload this page or relogin</p>
+export const Error500 = memo(() => <div className={style.error500__container}>
+    <h1 className={style.error500__title}>500</h1>
+    <p className={style.error500__text}>Server is not available</p>
+    <p className={style.error500__text}>try reload this page or relogin</p>
 </div>)
