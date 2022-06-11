09aa15a5fe9a769453c3750e8779a8541def1b61
voskanvl <voskanvl@gmail.com>
2022-06-11 10:55:15 +0300

    TButton -> IButton

--- src/features/button/Button.tsx
+++ src/features/button/Button.tsx
@@ -1,14 +1,14 @@
-import { FC, memo, MouseEventHandler } from "react"
+import { FC, InputHTMLAttributes, MouseEventHandler } from "react"
 import style from "./button.module.sass"
-type TButton = {
+interface IButton extends InputHTMLAttributes<HTMLInputElement> {
     preload?: boolean,
     error?: boolean,
     errorMessage?: string,
     label: string,
     onClick?: MouseEventHandler<HTMLInputElement>
 }
-export const Button: FC<TButton> = memo(({ preload, error, errorMessage, label, onClick }) => {
+export const Button: FC<IButton> = ({ preload, error, errorMessage, label, ...props }) => {
     return <div className={style.submit}>
-        <input className={`${style.submit__control} ${preload ? style.submit__preloading : ""} ${error ? style.submit__error : ""}`} type="submit" value={errorMessage ? errorMessage : label} onClick={onClick} />
+        <input className={`${style.submit__control} ${preload ? style.submit__preloading : ""} ${error ? style.submit__error : ""}`} type="submit" value={errorMessage ? errorMessage : label} onClick={props.onClick} {...props} />
     </div>
-})
+}
