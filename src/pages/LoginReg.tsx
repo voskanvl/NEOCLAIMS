a66dece6babb6e9ebd0973ba7df516da6e52c7f1
voskanvl <voskanvl@gmail.com>
2022-06-11 10:56:31 +0300

    insert validate

--- src/pages/LoginReg.tsx
+++ src/pages/LoginReg.tsx
@@ -1,4 +1,4 @@
-import { FC, FormEvent, FormEventHandler, useState, MouseEvent, useEffect } from "react"
+import { FC, MouseEvent, useEffect, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { Button } from "../features/button/Button"
 import { Input } from "../features/input/Input"
@@ -7,6 +7,7 @@ import { loginFetch, regFetch } from "../app/login"
 import { useAppDispatch, useAppSelector } from "../app/hooks"
 import { svg } from "../features/svg/svg"
 import { isTokenCorrect } from "../helpers/isTokenCorrect"
+import { useValidate, Valid } from "../hooks/useValidate"
 
 type TLoginReg = {
     fullNameInput?: boolean,
@@ -14,6 +15,10 @@ type TLoginReg = {
 export const LoginReg: FC<TLoginReg> = ({ fullNameInput }) => {
     const [{ fullName, email, password }, setAttribs] = useState<{ fullName?: string, email?: string, password?: string }>({ fullName: undefined, email: undefined, password: undefined })
     const [preload, setPreload] = useState<boolean>(false)
+
+    const { valid: validFullName, validate: validateFullName } = useValidate()
+    const { valid: validEMail, validate: validateEmail } = useValidate()
+    const { valid: validPassword, validate: validatePassword } = useValidate()
 
     const user = useAppSelector(state => state.login.user)
     const dispatch = useAppDispatch()
@@ -25,12 +30,16 @@ export const LoginReg: FC<TLoginReg> = ({ fullNameInput }) => {
             navigate('/claims')
     }, [user])
 
-    const handlerClick = (ev: MouseEvent<HTMLInputElement>) => {
-        ev.preventDefault()
-        if (fullName && email && password) dispatch(regFetch({ fullName, email, password }))
-        if (!fullName && email && password) dispatch(loginFetch({ email, password }))
+    const validForm = validFullName === Valid.valid && validEMail === Valid.valid && validPassword === Valid.valid
 
-        setPreload(true)
+    const submit = (ev: MouseEvent<HTMLInputElement>) => {
+        ev.preventDefault()
+        if (validForm) {
+            if (fullName && email && password) dispatch(regFetch({ fullName, email, password }))
+            if (!fullName && email && password) dispatch(loginFetch({ email, password }))
+            setPreload(true)
+        }
+
     }
     return <div className={style.screen}>
         <main className={style.layout}>
@@ -43,16 +52,43 @@ export const LoginReg: FC<TLoginReg> = ({ fullNameInput }) => {
                 <form className={style.panel}>
                     <div>{svg.sloganColor}</div>
                     {/* <Input svg={svg.name} label={"full name"} placeholder={"Type your full name"} onChange={ev => setAttribs(state => ({ ...state, fullName: ev.currentTarget.value }))} /> */}
-                    {fullNameInput && <Input label="full name" placeholder={"Type your full name"} onChange={ev => setAttribs(state => ({ ...state, fullName: ev.target.value }))} />}
-                    <Input svg={svg.email} label={"e-mail"} placeholder={"Type your e-mail"} onChange={ev => setAttribs(state => ({ ...state, email: ev.target.value }))} />
-                    <Input svg={svg.seal} label={"password"} placeholder={"Type your password"} onChange={ev => setAttribs(state => ({ ...state, password: ev.target.value }))} type="password" />
+                    {fullNameInput && <Input
+                        label="full name"
+                        placeholder={"Type your full name"}
+                        error={!!validFullName}
+                        errorName={'Ошибка'}
+                        onChange={ev => setAttribs(state => ({ ...state, fullName: ev.target.value }))}
+                        onInput={ev => validateFullName(ev.currentTarget.value, /[a-zA-Z|а-яёА-ЯЁ|\s]+/)}
+                        onBlur={ev => validateFullName(ev.currentTarget.value, /[a-zA-Z|а-яёА-ЯЁ|\s]+/)}
+                    />}
+                    <Input
+                        svg={svg.email}
+                        label={"e-mail"}
+                        placeholder={"Type your e-mail"}
+                        error={!!validEMail}
+                        errorName={'Ошибка'}
+                        onChange={ev => setAttribs(state => ({ ...state, email: ev.target.value }))}
+                        onInput={ev => validateEmail(ev.currentTarget.value, /\w.*@.+\..+/)}
+                        onBlur={ev => validateEmail(ev.currentTarget.value, /\w.*@.+\..+/)}
+                    />
+                    <Input
+                        svg={svg.seal}
+                        label={"password"}
+                        placeholder={"Type your password"}
+                        error={!!validPassword}
+                        errorName={'Ошибка'}
+                        onChange={ev => setAttribs(state => ({ ...state, password: ev.target.value }))}
+                        type="password"
+                        onInput={ev => validatePassword(ev.currentTarget.value, /[\w|\d]{6,}/)}
+                        onBlur={ev => validatePassword(ev.currentTarget.value, /[\w|\d]{6,}/)}
+                    />
                     <div className={style.checkbox}>
                         <div className={style.checkbox__decorator}>
                             <input className={style.checkbox__input} type="checkbox" />
                         </div>
                         <label className={style.checkbox__label}>Keep me logged in</label>
                     </div>
-                    <Button label="Login" onClick={handlerClick} preload={preload} error={!!user.error} errorMessage={user.error} />
+                    <Button label="Login" onClick={submit} preload={preload} error={!!user.error} errorMessage={user.error} disabled={(validFullName + validEMail + validPassword) !== 3} />
                     <div className={style.notAMember}>
                         Not a member? <Link to={"/reg"}>Request registration.</Link>
                     </div>
