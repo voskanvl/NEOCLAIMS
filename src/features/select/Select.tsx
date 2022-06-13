174727c19ff3a9a35d7f100ede4d034dc3c59bff
voskanvl <voskanvl@gmail.com>
2022-06-13 20:24:07 +0300

    Select fix

--- src/features/select/Select.tsx
+++ src/features/select/Select.tsx
@@ -12,15 +12,14 @@ type TSelect = {
 }
 
 export const Select: FC<TSelect> = memo(({ label, options, defaultValue, className, ...props }) => {
-    const { claimId } = useParams()
 
     return (
         <div className={`${style.select__wrapper} ${className}`} >
             <label className={style.select__label} >{label}</label>
             <div className={style.select__control} >
-                {claimId && <div className={style.mark} style={{ background: colorMap.Type.byName[defaultValue!] }}></div>}
-                <select {...props} className={claimId ? "" : style.shift} defaultValue={defaultValue}>
-                    {options && options.map(el => <option key={el} value={el} >{el}</option>)}
+                <div className={style.mark} style={{ background: colorMap.Type.byName[defaultValue!] }}></div>
+                <select {...props} className={style.select__shift} defaultValue={defaultValue}>
+                    {options && options.map(el => <option key={el} value={el} selected={el === defaultValue}>{el}</option>)}
                 </select>
             </div>
         </div>
