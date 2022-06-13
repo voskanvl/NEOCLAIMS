0c7d06e3bc03aa8e05a24322878a239945fbc8bb
voskanvl <voskanvl@gmail.com>
2022-06-13 11:30:17 +0300

    edits Layout modal

--- src/features/layout/Layout.tsx
+++ src/features/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, memo } from "react"
 import { FC } from "react"
+import { useAppSelector } from "../../app/hooks"
 import { Aside } from "../aside/Aside"
 import Header from "../header/Header"
 import style from "./layout.module.sass"
@@ -9,13 +10,17 @@ type TLayout = {
     children?: ReactNode
 }
 export const Layout: FC<TLayout> = memo(({ headerChildren, children }) => {
+    const { show } = useAppSelector(state => state.aside)
     return <div className={style.layout__layout}>
         <Aside />
         <main className={style.layout__main}>
             <Header>
                 {headerChildren}
             </Header>
-            {children}
+            <div className={`${style.layout__common}`} >
+                <div className={`${show && style.layout__show}`}></div>
+                {children}
+            </div>
         </main>
     </div>
 })
