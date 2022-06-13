5a33e15aea16839dc3b3a7a10f13f0b1815e4e99
voskanvl <voskanvl@gmail.com>
2022-06-13 14:36:56 +0300

    Create to create folder

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,6 @@
-import { Claim } from "./Claim";
+import { Claim } from "./claim/Claim";
 import { Claims } from "./Claims";
 import { LoginReg } from "./LoginReg";
-import { Create } from "./Create";
+import { Create } from "./create/Create";
 
 export default { Claim, Claims, Create, LoginReg };
