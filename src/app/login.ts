1d1372ecef03acedc510eae05c5a9c70a4fd749b
voskanvl <voskanvl@gmail.com>
2022-06-23 16:17:25 +0300

    fix error hander after login fetch

--- src/app/login.ts
+++ src/app/login.ts
@@ -1,19 +1,22 @@
-import { ActionReducerMapBuilder, createSlice } from "@reduxjs/toolkit";
-import { fullfilledHandler } from "./fullfilledHandler";
-import { loginFetch } from "./loginThunks/loginFetch";
-import { regFetch } from "./loginThunks/regFetch";
-import { currentUser } from "./loginThunks/currentUser";
-import { ActionPayloadError, CasesType, StateType } from "./types/loginTypes";
+import { ActionReducerMapBuilder, createSlice } from "@reduxjs/toolkit"
+import { fullfilledHandler } from "./fullfilledHandler"
+import { loginFetch } from "./loginThunks/loginFetch"
+import { regFetch } from "./loginThunks/regFetch"
+import { currentUser } from "./loginThunks/currentUser"
+import { ActionPayloadError, CasesType, StateType } from "./types/loginTypes"
 
 const inCaseError = (state: StateType, action: ActionPayloadError) => {
-    state.user.error =
-        action.error.name || action.error.message || action.error.code || "";
-};
+    console.log('action.payload', action.payload)
+    console.log('action.error', action.error)
+    // state.user.error =
+    //     action.error.code || action.error.message || action.error.name || ""
+    state.user.error = JSON.parse(action.payload.message).message
+}
 
 const cases: CasesType = {
     whenFullFilled: fullfilledHandler,
     whenError: inCaseError,
-};
+}
 
 export const loginSlice = createSlice({
     name: "user",
@@ -32,15 +35,15 @@ export const loginSlice = createSlice({
     },
     reducers: {},
     extraReducers: builder => {
-        asyncThunkCase(builder)(regFetch, cases);
-        asyncThunkCase(builder)(loginFetch, cases);
-        asyncThunkCase(builder)(currentUser, cases);
+        asyncThunkCase(builder)(regFetch, cases)
+        asyncThunkCase(builder)(loginFetch, cases)
+        asyncThunkCase(builder)(currentUser, cases)
     },
-});
+})
 
 const asyncThunkCase =
     (builder: ActionReducerMapBuilder<any>) =>
-    (thunk: any, cases: CasesType) => {
-        builder.addCase(thunk.fulfilled, cases.whenFullFilled);
-        builder.addCase(thunk.rejected, cases.whenError);
-    };
+        (thunk: any, cases: CasesType) => {
+            builder.addCase(thunk.fulfilled, cases.whenFullFilled)
+            builder.addCase(thunk.rejected, cases.whenError)
+        }
--- src/app/loginThunks/loginFetch.ts
+++ src/app/loginThunks/loginFetch.ts
@@ -1,5 +1,5 @@
-import { AsyncThunk, createAsyncThunk } from "@reduxjs/toolkit";
-import { request } from "../request";
+import { AsyncThunk, createAsyncThunk } from "@reduxjs/toolkit"
+import { request } from "../request"
 
 export const loginFetch: AsyncThunk<
     any,
@@ -9,9 +9,10 @@ export const loginFetch: AsyncThunk<
     "user/login",
     async ({ email, password }, { rejectWithValue }) => {
         try {
-            return await request("login", { email, password });
+            return await request("login", { email, password })
         } catch (error) {
-            return rejectWithValue(error);
+            const { message } = error as { message: string }
+            return rejectWithValue({ message })
         }
     },
-);
+)
