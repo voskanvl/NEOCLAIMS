868d4b8485746f5e4abcd164f8654dfde5cfc921
voskanvl <voskanvl@gmail.com>
2022-06-23 09:31:02 +0300

    change useMediaMatch

--- src/hooks/useMediaMatch.ts
+++ src/hooks/useMediaMatch.ts
@@ -1,12 +1,25 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { debounce } from "../helpers/debounce"
 
 export const useMediaMatch = (resolution: number) => {
     const requestMatch = () => matchMedia(`(max-width: ${resolution}px)`).matches
-    const [match, setMatch] = useState(requestMatch());
-    (window.onresize as Function) = debounce(() => {
+    const [match, setMatch] = useState(requestMatch())
+    // (window.onresize as Function) = debounce(() => {
+    //     const result = requestMatch()
+    //     setMatch(result)
+    // }, 200)
+
+    const handleMatch = useRef(debounce(() => {
         const result = requestMatch()
         setMatch(result)
-    }, 200)
+    }, 200))
+
+    useEffect(() => {
+        window.addEventListener("resize", handleMatch.current as EventListenerOrEventListenerObject)
+        return () => {
+            window.removeEventListener("resize", handleMatch.current as EventListenerOrEventListenerObject)
+        }
+    }, [requestMatch])
+
     return match
 }
