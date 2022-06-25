7deed30c050a544bc757dbc1b8dd5e4bbbc949e0
voskanvl <voskanvl@gmail.com>
2022-06-25 23:30:57 +0300

    type -> types

---
+++ src/types/types.ts
@@ -0,0 +1,52 @@
+// export type TOid = { $oid: string };
+export type TClaim = {
+    _id: string;
+    title: string;
+    description: string;
+    type: {
+        name: string;
+        slug: string;
+    };
+    status: {
+        name: string;
+        slug: string;
+    };
+    user: string;
+    createdAt: string | Date;
+    updatedAt: string | Date;
+};
+export type TRoles = {
+    _id: string;
+    name: string;
+    slug: string;
+    createdAt: { $date: Date };
+    updatedAt: { $date: Date };
+    _v: number;
+};
+export type TStatus = {
+    _id: string;
+    name: string;
+    slug: string;
+    createdAt: { $date: Date };
+    updatedAt: { $date: Date };
+    _v: number;
+};
+export type TTypes = {
+    _id: string;
+    name: string;
+    slug: string;
+    createdAt: { $date: Date };
+    updatedAt: { $date: Date };
+    _v: number;
+};
+export type TUsers = {
+    _id: string;
+    fullName: string;
+    email: string;
+    password: string;
+    role: string;
+    createdAt: { $date: Date };
+    updatedAt: { $date: Date };
+    _v: number;
+};
+export type Claim = TClaim & { type: string; status: string };
