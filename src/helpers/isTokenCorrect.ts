bb5fd373f2d227a135d5129d6c2e110fca3a3147
voskanvl <voskanvl@gmail.com>
2022-06-30 09:42:07 +0300

    fix isTokenCorrect

--- src/helpers/isTokenCorrect.ts
+++ src/helpers/isTokenCorrect.ts
@@ -2,10 +2,10 @@ export function isTokenCorrect(reset?: boolean): boolean {
     const token: string | null = localStorage.getItem("token")
     const created = localStorage.getItem("created")
     if (!token || !created) return false
-    const middle = token!.split(".")[1]
+    const middle = token?.split(".")[1]
     const toketJson = JSON.parse(atob(middle))
-    const expired = +created + toketJson.exp - toketJson.iat
-    const isLive = Date.now() / 1000 < expired
+    const expired = +created + toketJson.exp * 1000 - toketJson.iat * 1000
+    const isLive = Date.now() < expired
     if (!isLive && reset) {
         localStorage.removeItem("token")
         localStorage.removeItem("created")
