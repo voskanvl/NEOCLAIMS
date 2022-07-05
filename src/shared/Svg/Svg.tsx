1a352580fa7932883ac45de84026fccf1cbdcf66
voskanvl <voskanvl@gmail.com>
2022-07-05 09:18:40 +0300

    Svg with props

--- src/shared/Svg/Svg.tsx
+++ src/shared/Svg/Svg.tsx
@@ -1,76 +1,192 @@
-const Email = () => <svg viewBox="0 0 40 40" width="40" height="40" fill="none">
+import { SVGAttributes } from "react"
+
+const Email = (props: SVGAttributes<any>) => <svg
+    viewBox="0 0 40 40"
+    width="40"
+    height="40"
+    fill="none"
+    {...props}
+>
     <use xlinkHref="/icon-sprite.svg#mail"></use>
 </svg>
 
-const Seal = () => <svg viewBox="0 0 40 40" width="40" height="40" fill="none">
+const Seal = (props: SVGAttributes<any>) => <svg
+    viewBox="0 0 40 40"
+    width="40"
+    height="40"
+    fill="none"
+    {...props}
+>
     <use xlinkHref="/icon-sprite.svg#lock"></use>
 </svg>
 
-const SloganSvgFooter = () => <svg width="59.44" height="58" viewBox="0 0 165 161" fill="#fff">
+const SloganSvgFooter = (props: SVGAttributes<any>) => <svg
+    width="59.44"
+    height="58"
+    viewBox="0 0 165 161"
+    fill="#fff"
+    {...props}
+>
     <use xlinkHref="/icon-sprite.svg#sloganWhite"></use>
 </svg>
 
-const Burger = () => <svg viewBox="0 0 24 24" width="24" height="24" fill="none">
+const Burger = (props: SVGAttributes<any>) => <svg
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    fill="none"
+    {...props}
+>
     <use xlinkHref="/icon-sprite.svg#burger"></use>
 </svg>
 
-const Bell = () => <svg viewBox="0 0 40 40" width="40" height="40" fill="none">
+const Bell = (props: SVGAttributes<any>) => <svg
+    viewBox="0 0 40 40"
+    width="40"
+    height="40"
+    fill="none"
+    {...props}
+>
     <use xlinkHref="/icon-sprite.svg#bell"></use>
 </svg>
 
-const Logout = () => <svg viewBox="0 0 40 40" width="40" height="40" fill="none">
+const Logout = (props: SVGAttributes<any>) => <svg
+    viewBox="0 0 40 40"
+    width="40"
+    height="40"
+    fill="none"
+    {...props}
+>
     <use xlinkHref="/icon-sprite.svg#logout"></use>
 </svg>
 
-const SloganBig = () => <svg viewBox="0 0 165 161" width="165" height="161" fill="none">
+const SloganBig = (props: SVGAttributes<any>) => <svg
+    viewBox="0 0 165 161"
+    width="165"
+    height="161"
+    fill="none"
+    {...props}
+>
     <use xlinkHref="/icon-sprite.svg#sloganSmall"></use>
 </svg>
 
-const Slogan = () => <svg viewBox="0 0 72 72" width="72" height="72" fill="none">
+const Slogan = (props: SVGAttributes<any>) => <svg
+    viewBox="0 0 72 72"
+    width="72"
+    height="72"
+    fill="none"
+    {...props}
+>
     <use xlinkHref="/icon-sprite.svg#sloganSmall"></use>
 </svg>
 
-const Home = () => <svg viewBox="0 0 40 40" width="40" height="40" fill="none">
+const Home = (props: SVGAttributes<any>) => <svg
+    viewBox="0 0 40 40"
+    width="40"
+    height="40"
+    fill="none"
+    {...props}
+>
     <use xlinkHref="/icon-sprite.svg#home"></use>
 </svg>
 
-const Globe = () => <svg viewBox="0 0 40 40" width="40" height="40" fill="none">
+const Globe = (props: SVGAttributes<any>) => <svg
+    viewBox="0 0 40 40"
+    width="40"
+    height="40"
+    fill="none"
+    {...props}
+>
     <use xlinkHref="/icon-sprite.svg#globe"></use>
 </svg>
 
-const Archive = () => <svg viewBox="0 0 40 40" width="40" height="40" fill="none">
+const Archive = (props: SVGAttributes<any>) => <svg
+    viewBox="0 0 40 40"
+    width="40"
+    height="40"
+    fill="none"
+    {...props}
+>
     <use xlinkHref="/icon-sprite.svg#archive"></use>
 </svg>
 
-const PieChart = () => <svg viewBox="0 0 40 40" width="40" height="40" fill="none">
+const PieChart = (props: SVGAttributes<any>) => <svg
+    viewBox="0 0 40 40"
+    width="40"
+    height="40"
+    fill="none"
+    {...props}
+>
     <use xlinkHref="/icon-sprite.svg#pie-chart"></use>
 </svg>
 
-const Dollar = () => <svg viewBox="0 0 40 40" width="40" height="40" fill="none">
+const Dollar = (props: SVGAttributes<any>) => <svg
+    viewBox="0 0 40 40"
+    width="40"
+    height="40"
+    fill="none"
+    {...props}
+>
     <use xlinkHref="/icon-sprite.svg#dollar"></use>
 </svg>
 
-const Database = () => <svg viewBox="0 0 40 40" width="40" height="40" fill="none">
+const Database = (props: SVGAttributes<any>) => <svg
+    viewBox="0 0 40 40"
+    width="40"
+    height="40"
+    fill="none"
+    {...props}
+>
     <use xlinkHref="/icon-sprite.svg#database"></use>
 </svg>
 
-const Navigation = () => <svg viewBox="0 0 40 40" width="40" height="40" fill="none">
+const Navigation = (props: SVGAttributes<any>) => <svg
+    viewBox="0 0 40 40"
+    width="40"
+    height="40"
+    fill="none"
+    {...props}
+>
     <use xlinkHref="/icon-sprite.svg#navigation"></use>
 </svg>
 
-const Search = () => <svg viewBox="0 0 40 40" width="40" height="40" fill="none">
+const Search = (props: SVGAttributes<any>) => <svg
+    viewBox="0 0 40 40"
+    width="40"
+    height="40"
+    fill="none"
+    {...props}
+>
     <use xlinkHref="/icon-sprite.svg#search"></use>
 </svg>
 
-const SloganColor = () => <svg viewBox="0 0 165 161" width="165" height="161" fill="none">
+const SloganColor = (props: SVGAttributes<any>) => <svg
+    viewBox="0 0 165 161"
+    width="165"
+    height="161"
+    fill="none"
+    {...props}
+>
     <use xlinkHref="/icon-sprite.svg#sloganColor"></use>
 </svg>
 
-const Name = () => <svg viewBox="0 0 45.532 45.532" width="40" height="40" fill="#ADADAD">
+const Name = (props: SVGAttributes<any>) => <svg
+    viewBox="0 0 45.532 45.532"
+    width="40"
+    height="40"
+    fill="#ADADAD"
+    {...props}
+>
     <use xlinkHref="/icon-sprite.svg#name"></use>
 </svg>
 
-const Plus = () => <svg viewBox="0 0 40 40" width="40" height="40" fill="none">
+const Plus = (props: SVGAttributes<any>) => <svg
+    viewBox="0 0 40 40"
+    width="40"
+    height="40"
+    fill="none"
+    {...props}
+>
     <use xlinkHref="/icon-sprite.svg#plus"></use>
 </svg>
 
@@ -94,6 +210,6 @@ export const Svg = {
     Email,
     Name,
     Plus
-
 }
+
 export type TSvg = typeof Svg
